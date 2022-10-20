99f3db015fc4b0dab1d406d989232439bc26d56a
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-10-20 14:00:45 +0300

    navbar setup and assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+
+import logo from '../../images/logo.png'
+
+const NavbarItems = ({title, classProps}) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+          {title}
+        </li>
+    );
+
+}
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <img src={logo} alt='logo' className='w-32 cursor-pointer' />
+      </div>
+
+      <ul className='text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial'>
+        {["Market","Exchange","Tutorial","Wallets"].map((item, index) => (
+          <NavbarItems title={item} key={item+index} />
+        )   
+        )}
+
+        <li className='bg-[#2952e3] px-7 py-2 cursor-pointer mx-4 rounded-full hover:bg-[#2546bd]'>
+          Login
+        </li>
+      </ul>
+
+      <div className='flex relative'>
+        {navbarOpen 
+        ? <AiOutlineClose fontSize={28} onClick={() => setNavbarOpen(false)} className='text-white md:hidden cursor-pointer' /> 
+        : <HiMenuAlt4 fontSize={28} onClick={() => setNavbarOpen(true)} className='text-white md:hidden cursor-pointer' />}
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
