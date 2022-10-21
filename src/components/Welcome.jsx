23f55c395b04bd2db8a698dd2d39ed1a3320c9bd
Kerim <kerimberdi.agayev@edusoftech.eu>
2022-10-21 23:56:09 +0300

    welcome section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {AiFillPlayCircle} from 'react-icons/ai'
 import {SiEthereum} from 'react-icons/si'
 import {BsInfoCircle} from 'react-icons/bs'
+import { ReactComponent as Logo } from '../assets/logo.svg'
 
 import {Loader} from './'
 
@@ -9,15 +10,31 @@ const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-ce
 
 const Welcome = () => {
 
+  const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   const connectWallet = () => {
     console.log('connectWallet')
   }
 
   return (
     <div className='flex w-full justify-center items-center'>
-      <div className='flex md:flex-row flex-col items-start 
+      <div className='flex mf:flex-row flex-col items-start 
       justify-between md:p-20 py-12 px-4'>
-        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+        <div className='flex flex-1 justify-start flex-col mf:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
             Send Crypto <br/> across the world
           </h1>
@@ -57,13 +74,44 @@ const Welcome = () => {
         
 
         <div className='flex flex-col flex-1 items-center justify-start
-        w-full md:mt-0 mt-10'>
-          <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
-
+        w-full mf:mt-0 mt-10'>
+          <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 matic-card white-glassmorphism'>
+            <div className='flex justify-between w-full h-full flex-col'>
+              <div className='flex justify-between items-start'>
+                <div className='w-10 h-10 flex justify-center items-center'>
+                  <Logo />
+                </div>
+                <BsInfoCircle fontSize={17} color='#fff'/>
+              </div>
+              <div>
+                <p className='text-white font-light text-sm'>Polygon address</p>
+                <p className='text-white font-semibold text-lg mt-1'>Polygon</p>
+              </div>
+            </div>
           </div>
+
+          <div className='p-5 sm:w-96 w-full flex flex-col items-center justify-start blue-glassmorphism'>
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}} />
+            <Input placeholder="Amount (MATIC)" name="amount" type="number" handleChange={()=>{}} />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}} />
+
+            <div className='h-[1px] w-full bg-gray-400 my-2'/>
+
+            {false ? (
+              <Loader/>
+            ) : (
+              <button
+              type='button'
+              onClick={handleSubmit}
+              className='w-full mt-2 text-white border-[1px] border-[#3d4f7c] rounded-full p-2 cursor-pointer'
+              >
+                Send Now
+              </button>
+            )}
+          </div>
+
         </div>
-
-
       </div>
     </div>
   )
